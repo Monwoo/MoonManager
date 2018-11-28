ce10bc45013bec6c621ccc58bc1750e7c1e9049c
Mickael Moreau <mickael.moreau.pro@gmail.com>
2018-11-28 19:48:05 +0100

    Improving design

--- src/app/moon-manager/components/timing-form/timing-form.component.ts
+++ src/app/moon-manager/components/timing-form/timing-form.component.ts
@@ -1,14 +1,15 @@
 // Copyright Monwoo 2018, made by Miguel Monwoo, service@monwoo.com
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Timing } from '../../api/data-model/timing';
 
 @Component({
-  selector: 'app-timing-form',
+  selector: 'moon-manager-timing-form',
   templateUrl: './timing-form.component.html',
   styleUrls: ['./timing-form.component.scss']
 })
 export class TimingFormComponent implements OnInit {
+  @Input() t: Timing;
   constructor() {}
 
   ngOnInit() {}
