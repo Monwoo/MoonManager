d628af05d97549718dccb416271c90b252586699
Mickael Moreau <mickael.moreau.pro@gmail.com>
2018-11-28 23:28:39 +0100

    Improving design + ajust Parameters view with material designs components

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
-  defaultLanguage: 'en-US',
-  supportedLanguages: ['en-US', 'fr-FR']
+  serverUrl: '/MoonManager/Demo/',
+  defaultLanguage: 'fr-FR',
+  supportedLanguages: ['fr-FR', 'en-US']
 };
