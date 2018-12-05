3eef0fbfe85948fe1a7fa0c97f68a125c265f321
Mickael Moreau <mickael.moreau.pro@gmail.com>
2018-12-05 09:37:08 +0100

    Starting import/export capabilities

--- src/app/moon-manager/components/parameters/parameters.component.ts
+++ src/app/moon-manager/components/parameters/parameters.component.ts
@@ -15,12 +15,15 @@ import { LocalStorage } from '@ngx-pwa/local-storage';
 import { NotificationsService } from 'angular2-notifications';
 import { extract } from '@app/core';
 import { environment } from '@env/environment';
-import { CONFIG_FORM_LAYOUT, configFormModel, getFreshConf } from './config-form.model';
 import { DynamicFormModel, DynamicFormLayout, DynamicFormService } from '@ng-dynamic-forms/core';
 import { I18nService } from '@app/core';
 import { I18n } from '@ngx-translate/i18n-polyfill';
 import { BehaviorSubject } from 'rxjs';
 import { LangChangeEvent } from '@ngx-translate/core';
+import * as moment from 'moment';
+import { Papa } from 'ngx-papaparse';
+
+import { CONFIG_FORM_LAYOUT, configFormModel, getFreshConf } from './config-form.model';
 
 @Component({
   selector: 'moon-manager-parameters',
@@ -44,12 +47,38 @@ export class ParametersComponent implements OnInit, OnChanges, AfterViewInit {
   // but with below case, translator will not have easy ways to gess right translation...
   indicatorStoryUrl: string = extract('assets/TimingsIndicatorStory.png');
 
+  exportFmt: string = 'csv';
+  exportFmts: string[] = ['csv', 'json', 'yaml'];
+
+  dropzoneMediasConfig = {
+    url: '#', // Url set to avoid console Error, but will not be used in V1.0.0
+    autoProcessQueue: false, // We will no upload to server, only local processings for V1.0.0
+    autoQueue: false,
+    clickable: true,
+    acceptedFiles: '.json,.yaml,.csv',
+    accept: (f: any, isValidTrigger: any) => {
+      this.processImport(f, 'medias');
+    }
+  };
+
+  dropzoneTimingsConfig = {
+    url: '#', // Url set to avoid console Error, but will not be used in V1.0.0
+    autoProcessQueue: false, // We will no upload to server, only local processings for V1.0.0
+    autoQueue: false,
+    clickable: true,
+    acceptedFiles: '.json,.yaml,.csv',
+    accept: (f: any, isValidTrigger: any) => {
+      this.processImport(f, 'timings');
+    }
+  };
+
   constructor(
     private ngZone: NgZone,
     private storage: LocalStorage,
     private i18nService: I18nService,
     private notif: NotificationsService,
     private formService: DynamicFormService,
+    private papaParse: Papa,
     public i18n: I18n
   ) {}
 
@@ -155,5 +184,78 @@ export class ParametersComponent implements OnInit, OnChanges, AfterViewInit {
 
   get languages(): string[] {
     return this.i18nService.supportedLanguages;
+  }
+
+  processExport(dest: string) {
+    // TODO : auto download right format
+  }
+
+  processImport(f: File, dest: string) {
+    const fileName = f.name;
+    const reader: FileReader = new FileReader();
+    reader.onload = e => {
+      if (dest === 'medias') {
+      } else if (dest === 'timings' && fileName.match(/.*\.csv$/i)) {
+        const csv: string = <string>reader.result;
+        const parsed = this.papaParse.parse(csv, { header: false });
+        console.log('TODO : process git log datas : ', parsed);
+        // TODO : scheme checking ? what if bad format ?
+        // this.processInc(null, parsed.data.length);
+        parsed.data.forEach((row: string[]) => {
+          // let t = new Timing();
+          // let date = moment(row[2], ''); // TODO : regex extract from path
+          // let segmentDelta = 1; // TODO : curently hard coded, need to be loaded with eventSource
+          // let segmentOverride = Math.floor((date.hour() + date.minute() / 60) / segmentDelta);
+          // t.id = ++this.index;
+          // t.DateTime = date.toDate();
+          // t.EventSource = 'git-log'; // TODO : configurable from parameters ?
+          // t.ExpertiseLevel = row.length > 8 ? row[8] : '';
+          // t.Project = row.length > 5 ? row[5] : '';
+          // t.SubProject = row.length > 6 ? row[6] : '';
+          // t.Objectif = row.length > 7 ? row[7] : '';
+          // t.Comment = row[3];
+          // t.Title = t.Comment.substring(0, 100);
+          // t.MediaUrl = row[0];
+          // t.Author = row.length > 4 ? row[4] : this.config.timingAuthor;
+          // t.ReviewedComment = '';
+          // t.OverrideSequence = '';
+          // t.OverrideReduction = '';
+          // t.SegmentOverride = segmentOverride;
+          // // t.SegmentMin = minDate; TODO : refactor, not really used
+          // t.SegmentDeltaHr = segmentDelta;
+          // t.SegmentMax = date.toDate();
+          // t.Date = date.format('YYYY/MM/DD');
+          // t.Time = date.format('HH:mm:ss');
+          // t.Month = '';
+          // t.Year = '';
+          // // Client side computed fields :
+          // t.LinearWorkloadAmount = 0;
+          // t.WorkloadAmount = 0;
+          // t.TJM = 0;
+          // t.TJMWorkloadByDay = 0;
+          // t.Price = 0;
+          // t.isHidden = false;
+          // this.onTimingFetch.emit(t);
+          // this.processDec(this.getFilePath(f));
+        });
+      } else {
+        console.log('Unknow import dest : ', dest);
+        return;
+      }
+      // this.processDec(this.getFilePath(f));
+    };
+    reader.onabort = (ev: ProgressEvent) => {
+      console.log('Aborting : ', f);
+      // this.processDec(this.getFilePath(f));
+    };
+    reader.onerror = (ev: ProgressEvent) => {
+      console.error('Error for : ', f);
+      // this.processDec(this.getFilePath(f));
+    };
+    reader.readAsText(f);
+  }
+
+  onUploadError(e: any, dst: string) {
+    console.log('Upload error', e);
   }
 }
